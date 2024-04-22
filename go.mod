6d9e34f8807843c525fc3cfa2a1578813031028a
hongdacai <1526875642@qq.com>
2024-04-22 10:16:13 +0800

    feat: test bdb. --story=00000

--- go.mod
+++ go.mod
@@ -18,7 +18,9 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.etcd.io/bbolt v1.3.7 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,14 +2,49 @@ package main
 
 import (
 	"fmt"
+	"github.com/btcsuite/btcd/btcutil/hdkeychain"
+	"github.com/czh0526/btc-wallet/key"
+	"github.com/czh0526/btc-wallet/seed"
 	"os"
+
+	"github.com/czh0526/btc-wallet/wallet"
 )
 
 func main() {
 	currDir, err := os.Getwd()
 	if err != nil {
-		panic(fmt.Sprintf("获取当前路径失败：err = %v"))
+		panic(fmt.Sprintf("获取当前路径失败：err = %v", err))
 	}
 
-	walletdb := wallet.NewDB(currDir)
+	walletDir, err := wallet.CreateDir(currDir)
+	if err != nil {
+		panic(fmt.Sprintf("创建钱包文件夹失败：err = %v", err))
+	}
+
+	walletDB, isNew, err := wallet.OpenDB(walletDir)
+	if err != nil {
+		panic(fmt.Sprintf("创建钱包数据库失败：err = %v", err))
+	}
+
+	if isNew {
+		var seedBytes []byte
+		var rootKey *hdkeychain.ExtendedKey
+
+		seedBytes, err = seed.NewSeed()
+		if err != nil {
+			panic(fmt.Sprintf("生成随机数失败: %v", err))
+		}
+
+		rootKey, err = key.NewRootKey(seedBytes)
+		if err != nil {
+			panic(fmt.Sprintf("生成`RootKey`失败: %v", err))
+		}
+	}
+
+	defer func() {
+		err = walletDB.Close()
+		if err != nil {
+			panic(fmt.Sprintf("关闭钱包数据库失败：err = %v", err))
+		}
+	}()
 }
